0be1027abe090d7fa418d0cb6e1ec43a794c41ef
hasan2314124 <hasanrahal2021@gmail.com>
2024-07-17 20:49:29 +0300

    2

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,10 +3,7 @@ class Driver:
         self.worker_id = worker_id
         self.name = name
         self.start_city = start_city
-    class City:
-     def __init__(self, city_name, reachable_cities):
-        self.city_name = city_name
-        self.reachable_cities = reachable_cities
+   
 class wedeliversystem:  
     def __init__(self) :
         self.driver = {
