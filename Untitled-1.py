344b304e3ae38aeff69fba9b597e176905def218
hasan2314124 <hasanrahal2021@gmail.com>
2024-07-17 20:51:24 +0300

    2

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,3 +4,8 @@ class Driver:
         self.name = name
         self.start_city = start_city
    
+class City:
+     def __init__(self, city_name, reachable_cities):
+        self.city_name = city_name
+        self.reachable_cities = reachable_cities
+        
