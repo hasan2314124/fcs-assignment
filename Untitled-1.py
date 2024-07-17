537cdb2796c8b292a7f1057f8792d00c4783ade3
hasan2314124 <hasanrahal2021@gmail.com>
2024-07-17 20:52:17 +0300

    3

--- Untitled-1.py
+++ Untitled-1.py
@@ -9,3 +9,13 @@ class City:
         self.city_name = city_name
         self.reachable_cities = reachable_cities
         
+class wedeliversystem:  
+    def __init__(self) :
+        self.driver = {
+         "ID001": {"name": "max verstappen","start_city":"beirut"},
+         "iD002": {"name": "Charles Leclerc,","start_city":"tripoli"},
+         "iD003": {"name": "lando Norris","start_city":"zahle"},
+         "iD004": {"name": "Sebstian Vettal","start_city":"saida"},
+         "ID005": {"name": "lewis hamilton","start_city":"Byblos"},
+        }
+        
