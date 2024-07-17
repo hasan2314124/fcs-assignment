bf290fb5ad2a3dc18caaf70f81f2c4eeacc8f663
hasan2314124 <hasanrahal2021@gmail.com>
2024-07-17 20:50:38 +0300

    1

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,23 +4,3 @@ class Driver:
         self.name = name
         self.start_city = start_city
    
-class wedeliversystem:  
-    def __init__(self) :
-        self.driver = {
-         "ID001": {"name": "max verstappen","start_city":"beirut"},
-         "iD002": {"name": "Charles Leclerc,","start_city":"tripoli"},
-         "iD003": {"name": "lando Norris","start_city":"zahle"},
-         "iD004": {"name": "Sebstian Vettal","start_city":"saida"},
-         "ID005": {"name": "lewis hamilton","start_city":"Byblos"},
-            
-        }
-        self.cities ={
-            "beirut":["saida","byblos"],
-            "tripoli":["beirut","jounieh"],
-            "zahle":["chtoura","saoufar"],
-            "saida":["sour","jeye"],
-            "byblos":["jounieh","amchit"],
-            }
-        
-        
-        
