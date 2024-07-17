78baaf64927937e2d7fef4d58a7bfcff129a5b7a
hasan2314124 <hasanrahal2021@gmail.com>
2024-07-17 19:41:20 +0300

    fcs project

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+class wedeliversystem:  
+    def __init__(self) :
+        self.driver = {
+         "ID001": {"name": "max verstappen","start_city":"beirut"},
+         "iD002": {"name": "Charles Leclerc,","start_city":"tripoli"},
+         "iD003": {"name": "lando Norris","start_city":"zahle"},
+         "iD004": {"name": "Sebstian Vettal","start_city":"saida"},
+         "ID005": {"name": "lewis hamilton","start_city":"Byblos"},
+            
+        }
+       
